5cdd9c88e864bd26ae84bd2de2dff4870d950a30
François Billet <francoisbillet@live.fr>
2022-07-21 18:51:14 -0400

    Resolve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Remove uppercase letters and punctuation
+  // Transform spaces into a single space
+  // Store resulting string in variable
+  // Turn string into array and reverse array
+  // Convert reversed array to a string variable
+  // compare both variables
 
+  const modifiedString = string.toLowerCase().replace(/[^a-z]/g, "");
+
+  const reversedModifiedString = modifiedString.split("").reverse().join("");
+
+  return modifiedString === reversedModifiedString;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes("ZZZZ car, a man, a maracaz."));
