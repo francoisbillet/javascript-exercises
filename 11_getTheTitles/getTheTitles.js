73b76c8a9759df339c10dbbc68e8e26cc4bad079
François Billet <francoisbillet@live.fr>
2022-07-22 16:14:24 -0400

    Resolve getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  return books.map((book) => book.title);
 };
+
+// const getTheTitles = function (books) {
+//   const titlesArray = [];
+//   books.forEach((book) => {
+//     titlesArray.push(book.title);
+//   });
+//   return titlesArray;
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
