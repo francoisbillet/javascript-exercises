8d3fe6ed6812cb3af2802c11f75bc3e6edb12370
François Billet <francoisbillet@live.fr>
2022-07-22 17:07:07 -0400

    Realise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,51 @@
-const findTheOldest = function() {
+// const findTheOldest = function (people) {
+//   people.sort((person1, person2) => {
+//     const person1Age =
+//       (person1.yearOfDeath ?? new Date().getFullYear()) - person1.yearOfBirth;
+//     const person2Age =
+//       (person2.yearOfDeath ?? new Date().getFullYear()) - person2.yearOfBirth;
+//     return person1Age > person2Age ? -1 : 1;
+//   });
 
+//   return people[0];
+// };
+
+// const findTheOldest = (people) => {
+//   return people.reduce((personWithMaxAge, person) => {
+//     if (Object.keys(personWithMaxAge).length === 0) {
+//       return person;
+//     }
+//     const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+//     const personWithMaxAgeAge = getAge(
+//       personWithMaxAge.yearOfBirth,
+//       personWithMaxAge.yearOfDeath
+//     );
+
+//     if (personAge > personWithMaxAgeAge) {
+//       personWithMaxAge = person;
+//     }
+//     return personWithMaxAge;
+//   }, {});
+// };
+
+const getAge = (yearOfBirth, yearOfDeath) => {
+  return (yearOfDeath ?? new Date().getFullYear()) - yearOfBirth;
+};
+
+// Even better:
+const findTheOldest = (people) => {
+  return people.reduce((personWithMaxAge, person) => {
+    const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+    const personWithMaxAgeAge = getAge(
+      personWithMaxAge.yearOfBirth,
+      personWithMaxAge.yearOfDeath
+    );
+
+    if (personAge > personWithMaxAgeAge) {
+      personWithMaxAge = person;
+    }
+    return personWithMaxAge;
+  });
 };
 
 // Do not edit below this line
